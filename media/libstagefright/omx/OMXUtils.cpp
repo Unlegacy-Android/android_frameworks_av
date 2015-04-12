c2f134c4df839ce0f54763891c2a2d78c7f7c82b
Ziyan <jaraidaniel@gmail.com>
2015-04-12 21:02:20 +0200

    libstagefright: add missing TI color format case
    Change-Id: Iaca45e6b15d3315a39b51bbf7f0cf3a1e912fee9
    Moved from media/libstagefright/ACodec.cpp to
    media/libstagefright/omx/OMXUtils.cpp with Oreo.
    Signed-off-by: Andreas Blaesius <skate4life@gmx.de>
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- media/libstagefright/omx/OMXUtils.cpp
+++ media/libstagefright/omx/OMXUtils.cpp
@@ -215,6 +215,7 @@ bool DescribeDefaultColorFormat(DescribeColorFormat2Params &params) {
         fmt != OMX_COLOR_FormatYUV420PackedPlanar &&
         fmt != OMX_COLOR_FormatYUV420SemiPlanar &&
         fmt != OMX_COLOR_FormatYUV420PackedSemiPlanar &&
+        fmt != OMX_TI_COLOR_FormatYUV420PackedSemiPlanar &&
         fmt != (OMX_COLOR_FORMATTYPE)HAL_PIXEL_FORMAT_YV12) {
         ALOGW("do not know color format 0x%x = %d", fmt, fmt);
         if (fmt == OMX_COLOR_FormatYUV420Planar16) {
@@ -294,6 +295,7 @@ bool DescribeDefaultColorFormat(DescribeColorFormat2Params &params) {
         case OMX_COLOR_FormatYUV420SemiPlanar:
             // FIXME: NV21 for sw-encoder, NV12 for decoder and hw-encoder
         case OMX_COLOR_FormatYUV420PackedSemiPlanar:
+        case OMX_TI_COLOR_FormatYUV420PackedSemiPlanar:
             // NV12
             image.mPlane[image.U].mOffset = params.nStride * params.nSliceHeight;
             image.mPlane[image.U].mColInc = 2;
